a2a95d497f2199bd90c38c5a7c25566ed90949a7
Hanne Moa <hanne.moa@uninett.no>
2017-12-13 10:20:58 +0100

    Bumped version, for tagging

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as F:
 
 setup(
     name='social-auth-backend-b2access',
-    version='0.1.0',
+    version='1.0.0',
     py_modules=['b2access'],
     include_package_data=True,
     package_dir = {'': 'src',},
